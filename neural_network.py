77fa9e65b1700d2ea4a04114a2a95430b9dfc0f4
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-02-26 18:37:41 -0600

    Activation Function

--- neural_network.py
+++ neural_network.py
@@ -7,5 +7,10 @@ weights = np.around(np.random.uniform(size=2), decimals=2)
 
 biases = np.around(np.random.uniform(size=1), decimals=2)
 
-z = (x1 * weights[0]) + (x2 * weights[1]) + biases[0]
-print(z)
+z = (x1 * weights[0]) + (x2 * weights[1]) + biases[0] #Weighted sum
+
+print(f'Weighted sum: {z}')
+
+a = 1/ (1+ np.exp(-z)) #Activation Function
+
+print(f'Activation Funtion Output: {a}')
