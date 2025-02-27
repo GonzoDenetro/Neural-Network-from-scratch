bdf511d25544cccf1706b67f02e869b2b421c55d
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-02-26 18:55:15 -0600

    Function for activation function

--- neural_network.py
+++ neural_network.py
@@ -10,4 +10,21 @@ def weighted_sum(x, weights, biases):
     z =  np.sum(x * weights) + biases
     return z
 
-print(weighted_sum(x, weights, biases))
+def activation_function(weights_sum, activation_type):
+    if activation_type == 'sigmoid':
+        return 1 / (1 + np.exp(-weights_sum))
+    elif activation_type == 'tanh':
+        return (np.exp(weights_sum) -  np.exp(-weights_sum)) / (np.exp(weights_sum) +  np.exp(weights_sum))
+    elif activation_type == 'relu':
+        if weights_sum > 0:
+            return weights_sum
+        else:
+            return 0
+
+activation = ['sigmoid', 'tanh', 'relu']
+
+
+for type in activation:
+    z = weighted_sum(x, weights, biases)
+    print(f'{type} - {activation_function(z, type)}')
+    
