efbe1e249dd2b3c66b0f3f0991e74dab8b251c53
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-02-28 10:26:21 -0600

    Function to initialize network, the numbers of weights it gonna depend on the number of inputs and neurons. Considering only one layer

--- neural_network.py
+++ neural_network.py
@@ -3,7 +3,6 @@ import numpy as np
 x = np.around(np.random.uniform(size=2), decimals=2)
 
 weights = np.around(np.random.uniform(size=4), decimals= 2)
-print(x.shape[0])
 
 biases = np.around(np.random.uniform(size=1), decimals=2)
 
@@ -30,10 +29,48 @@ def initialize(n_layers, n_neurons, x_inputs):
     network['biases'] = np.random.rand(n_neurons)
     
     network['layers'] = n_layers
-    print(network)
-    
 
-create_layer(1, 3, x)
+    
+def neuron(inputs, weights, biases):
+    z = weighted_sum(inputs, weights, biases)
+    return activation_function(z, 'sigmoid')
+
+def layer_ouput(n_neurons, inputs):
+    weights = np.random.rand(n_neurons, inputs.shape[0])
+    biases = np.random.rand(n_neurons)
+    output = neuron(inputs, weights, biases)
+    return [weights, biases, output]
+    
+def network(n_layers, inputs, n_neurons):
+    my_network = {}
+    
+    for i in range(n_layers):
+        layer = {}
+        layer_name = f'layer-{i}'
+        layer['name'] = layer_name
+        if i == 0:
+            l_ouput = layer_ouput(n_neurons[i], inputs)
+            l_weights = l_ouput[0]
+            layer['weights'] = l_weights
+            l_biases = l_ouput[1]
+            layer['biases'] = l_biases
+            layer_output = l_ouput[2]
+            layer['output'] = layer_output
+        else:
+            l_ouput = layer_ouput(n_neurons[i], layer_output)
+            l_weights = l_ouput[0]
+            layer['weights'] = l_weights
+            l_biases = l_ouput[1]
+            layer['biases'] = l_biases
+            layer_output = l_ouput[2]
+            layer['output'] = layer_output
+        my_network[layer_name] = layer 
+        return my_network       
+            
+        
+
+print(''*20)
+print(network(3, x, [3, 3, 2]))
     
     
 
