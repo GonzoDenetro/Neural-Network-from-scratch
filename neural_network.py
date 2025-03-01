abc7950776c93a35da7028b2318a104a0a6d9cea
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-02-28 19:03:01 -0600

    details modified

--- neural_network.py
+++ neural_network.py
@@ -5,7 +5,7 @@ x = np.around(np.random.uniform(size=2), decimals=2)
 
 #SUM OF WEIGHTS
 def weighted_sum(x, weights, biases):
-    z =  np.sum(x * weights) + biases
+    z =  np.sum(x.dot(weights.T)) + biases
     return z
 
 #ACTIVATION FUNCTION
@@ -13,7 +13,7 @@ def activation_function(weights_sum, activation_type):
     if activation_type == 'sigmoid':
         return 1 / (1 + np.exp(-weights_sum))
     elif activation_type == 'tanh':
-        return (np.exp(weights_sum) -  np.exp(-weights_sum)) / (np.exp(weights_sum) +  np.exp(weights_sum))
+        return (np.exp(weights_sum) -  np.exp(-weights_sum)) / (np.exp(weights_sum) +  np.exp(-weights_sum))
     elif activation_type == 'relu':
         if weights_sum > 0:
             return weights_sum
@@ -49,13 +49,13 @@ def network(n_layers, inputs, n_neurons):
         layer['weights'] = l_ouput[0]
         layer['biases'] = l_ouput[1]
         result = l_ouput[2]
-        layer['output-a{i}'] = result
+        layer[f'output-a{i}'] = result
         my_network[layer_name] = layer
         
     return my_network       
             
         
 n_network = network(n_layers=3, inputs=x, n_neurons=[3, 3, 2])
-print(n_network)
+print(n_network['layer-0'])
 
     
