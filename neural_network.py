26639b114b695b422f4ca6a6f12607616647115c
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-04-10 09:48:04 -0600

    Code Optimization, layers class added, and loss function

--- neural_network.py
+++ neural_network.py
@@ -3,86 +3,78 @@ import numpy as np
 np.random.seed(42)
 
 
-class NeuralNetwork():
-    def __init__(self, n_neurons, n_layers, x_inputs):
-        self.n_neurons = n_neurons
-        self.n_layers = n_layers
-        self.weights = []
-        self.biases = []
-        self.network = {}
+class Layer():
+    def __init__(self, neurons, activation, x_inputs):
+        self.neurons = neurons
+        self.activation = activation
         self.x_inputs = x_inputs
-    
-    def __str__(self):
-        text = ""
-        text = f'Input layer: {len(self.x_inputs)} neurons. '
-        for i in range(len(self.n_neurons)):
-            if i == self.n_layers-1:
-                text += f'Output layer: {self.n_neurons[i]} neurons. '
-            else:
-                text += f'Hidden layer {i+1}: {self.n_neurons[i]} neurons. '
-        return text
-                
-    
-    
-    #SUM OF WEIGHTS
-    def weighted_sum(self, x, weights, biases):
-        z =  x.dot(weights.T) + biases
-        return z
-
-    #ACTIVATION FUNCTION
-    def activation_function(self, weights_sum, activation_type):
-        if activation_type == 'sigmoid':
-            return 1 / (1 + np.exp(-weights_sum))
-        elif activation_type == 'tanh':
-            return (np.exp(weights_sum) -  np.exp(-weights_sum)) / (np.exp(weights_sum) +  np.exp(-weights_sum))
-        elif activation_type == 'relu':
-            if weights_sum > 0:
-                return weights_sum
-            else:
-                return 0
-            
-    #This function represents the operation that a neuron performs    
-    def neuron_outputs(self, inputs, weights, biases):
-        z = self.weighted_sum(inputs, weights, biases)
-        return self.activation_function(z, 'sigmoid')
-
-    #This function creates the parameters that the layer will need, the parameters depends on the number of neurons and the inputs we have
-    def layer_ouput(self, neurons, inputs):
-        #print(f'inputs shape: {inputs.shape}')
-        #print(f'weights shape: {neurons}, {inputs.shape[0]}')
-        weights = np.random.rand(neurons, inputs.shape[0])
-        biases = np.random.rand(neurons)
-        output = self.neuron_outputs(inputs, weights, biases)
-        return [weights, biases, output]
-    
-    def forward_propagation(self, inputs):
-        for i in range(self.n_layers):
-            layer = {}
-            layer_name = f'layer-{i}'
-            layer['name'] = layer_name
-            
-            if i == 0:
-                l_ouput = self.layer_ouput(self.n_neurons[i], inputs)
-            else:
-                l_ouput = self.layer_ouput(self.n_neurons[i], result) #The output of the past layer is the input for the next layer
-            
-            layer['weights'] = l_ouput[0]
-            self.weights = l_ouput[0]
-            
-            layer['biases'] = l_ouput[1]
-            self.biases = l_ouput[1]
-            
-            result = l_ouput[2]
-            layer[f'output-a{i}'] = result
-            self.network[layer_name] = layer
-
+        self.weights = np.random.rand(neurons, x_inputs.shape[0])
+        self.biases = np.random.rand(neurons)
+        self._values = np.zeros(neurons)
         
+    def weighted_sum(self):
+        return self.x_inputs.dot(self.weights.T) + self.biases
+    
+    def activation_function(self, weights_sum):
+        try:
+            if self.activation == 'sigmoid':
+                return 1 / (1 + np.exp(-weights_sum))
+            elif self.activation == 'tanh':
+                return (np.exp(weights_sum) -  np.exp(-weights_sum)) / (np.exp(weights_sum) +  np.exp(-weights_sum))
+            elif self.activation == 'relu':
+                return np.maximum(0, weights_sum)
+            elif self.activation == 'softmax':
+                return np.exp(weights_sum) / np.sum(np.exp(weights_sum))
+        except:
+            print('No activation Function Found')
+    
+    def forward_propagation(self):
+        weights_sum = self.weighted_sum()
+        self._values = self.activation_function(weights_sum)
+
+    def values(self):
+        return self._values
+
+
+class NeuralNetwork():
+    def __init__(self):
+        self._layers = []
+        self._output = []
+    
+    def add_layer(self, layer):
+        self._layers.append(layer)
+    
+    def layers(self):
+        return self._layers    
+    
+    def propagate_network(self):
+        for layer in self._layers:
+            layer.forward_propagation()
+            
+    def output(self):
+        last_layer = len(self._layers) - 1
+        self._output = self._layers[last_layer].values()
+        return self._output
+    
+    def loss_function(self, y_values):
+        cross_entroy = -1 * np.sum(y_values * np.log(self._output))
+        return cross_entroy
+
 x = np.around(np.random.uniform(size=2), decimals=2)
-       
-        
-n_network = NeuralNetwork(n_neurons=[3, 3, 2], n_layers=3, x_inputs=x)
-n_network.forward_propagation(inputs=x)
-print(n_network)
-print(n_network.network)
+print(x)
 
+layer_1 = Layer(3, 'relu', x)
+layer_2 = Layer(3, 'relu', layer_1.values())
+layer_3 = Layer(2, 'softmax', layer_2.values())
+
+network = NeuralNetwork()
+network.add_layer(layer_1)
+network.add_layer(layer_2)
+network.add_layer(layer_3)
+network.propagate_network()
+
+
+print(network.output())
+print(network.loss_function([0, 1]))
+        
     
