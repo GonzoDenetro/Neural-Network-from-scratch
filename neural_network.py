3e66177195d50176fe66b16a8847e84cd61bf530
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-04-25 15:01:05 -0600

    Backpropagation for last layer ready

--- neural_network.py
+++ neural_network.py
@@ -4,16 +4,15 @@ np.random.seed(42)
 
 
 class Layer():
-    def __init__(self, neurons, activation, x_inputs):
+    def __init__(self, neurons, n_inputs, activation):
         self.neurons = neurons
         self.activation = activation
-        self.x_inputs = x_inputs
-        self.weights = np.random.rand(neurons, x_inputs.shape[0])
+        self.weights = np.random.rand(neurons, n_inputs)
         self.biases = np.random.rand(neurons)
         self._values = np.zeros(neurons)
         
-    def weighted_sum(self):
-        return self.x_inputs.dot(self.weights.T) + self.biases
+    def weighted_sum(self, x_inputs):
+        return x_inputs.dot(self.weights.T) + self.biases
     
     def activation_function(self, weights_sum):
         try:
@@ -28,18 +27,28 @@ class Layer():
         except:
             print('No activation Function Found')
     
-    def forward_propagation(self):
-        weights_sum = self.weighted_sum()
+    def forward_propagation(self, x_inputs):
+        weights_sum = self.weighted_sum(x_inputs)
         self._values = self.activation_function(weights_sum)
 
     def values(self):
         return self._values
+    
+    def softmax_derivate(self, output):
+        return output * (1 - output)
+
+    def weights_sum_derivate(self):
+        print(f'jjfjfjf {self.x_inputs}')
+        return self.x_inputs.T
+        
 
 
 class NeuralNetwork():
-    def __init__(self):
+    def __init__(self, x_inputs, y_train):
         self._layers = []
         self._output = []
+        self.x_inputs = x_inputs 
+        self.y_train = np.array(y_train)
     
     def add_layer(self, layer):
         self._layers.append(layer)
@@ -48,33 +57,67 @@ class NeuralNetwork():
         return self._layers    
     
     def propagate_network(self):
-        for layer in self._layers:
-            layer.forward_propagation()
+        for i, layer in enumerate(self._layers):
+            if i == 0:
+                layer.forward_propagation(x_inputs=self.x_inputs)
+            else:
+                layer.forward_propagation(x_inputs=self._layers[i-1].values())
             
     def output(self):
         last_layer = len(self._layers) - 1
         self._output = self._layers[last_layer].values()
         return self._output
     
-    def loss_function(self, y_values):
-        cross_entroy = -1 * np.sum(y_values * np.log(self._output))
+    def loss_function(self):
+        cross_entroy = -1 * np.sum(self.y_train * np.log(self._output))
         return cross_entroy
+    
+    def cross_entropy_derivate(self):
+        return - self.y_train / self._output
+    
+    def backpropgation(self):
+        last_layer = len(self._layers) - 1
+        print(len(self._layers))
+        output = self.output()
+        learning_rate = 0.001
+        
+        for i in range(last_layer, -1, -1):
+            if i == last_layer:
+                de_cost = self.cross_entropy_derivate()
+                de_activation = self._layers[i].softmax_derivate(output)
+                inputs = self._layers[i-1].values()
+                print(f'Inputs Layer: {i}, shape {inputs.shape}')
+                print(self._layers[i-1].values())
+                print(f'derivate cost: {de_cost}')
+                print(f'derivate activation: {de_activation}')
+                layer_error = np.dot(de_cost, de_activation)
+                print(f'Layer error: {layer_error}')
+                gradient = np.dot(layer_error, inputs)
+                print(f'Gradient: {gradient}')
+                weights = self._layers[i].weights
+                print(f'Weights: {weights}')
+                tetha = self._layers[i].weights - learning_rate*gradient
+                print(f'NEW thetha: {tetha}')
+            else:
+                pass
 
 x = np.around(np.random.uniform(size=2), decimals=2)
-print(x)
 
-layer_1 = Layer(3, 'relu', x)
-layer_2 = Layer(3, 'relu', layer_1.values())
-layer_3 = Layer(2, 'softmax', layer_2.values())
+layer_1 = Layer(neurons=3, n_inputs=2,  activation='relu')
+layer_2 = Layer(neurons=3,  n_inputs=3, activation='relu')
+layer_3 = Layer(neurons=2, n_inputs=3, activation='softmax')
 
-network = NeuralNetwork()
+network = NeuralNetwork(x_inputs=x, y_train=[0, 1])
 network.add_layer(layer_1)
 network.add_layer(layer_2)
 network.add_layer(layer_3)
 network.propagate_network()
 
 
-print(network.output())
-print(network.loss_function([0, 1]))
-        
-    
+
+print(f'FINAL OUTPUT: {network.output()}')
+print(f'Loss: {network.loss_function()}')
+network.backpropgation()
+#print(f'Tetha: {network.backpropgation()}')
+
+print(f'Layer 1 OUTPUT {layer_1._values}') 
