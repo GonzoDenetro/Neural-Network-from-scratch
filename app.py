a564e9231d8aacf803d98160a9efde6a815a5109
Gonzo Denetro <gonzi.ceron.denetro@gmailcom>
2025-05-19 14:31:41 -0600

    Comments added

--- app.py
+++ app.py
@@ -9,56 +9,34 @@ data = load_iris()
 X = data.data
 Y = data.target #  (0 = setosa, 1 = versicolor, 2 = virginica)
 values = data.target_names
-#print(values)
-#print(Y)
 
 #SCALE DATA
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
-#print(X_scaled)
 
 #ONE HOT ENCODING
 encoder = OneHotEncoder(sparse_output=False)
 y_onehot = encoder.fit_transform(Y.reshape(-1, 1))
-#print(y_onehot)
 
 #SPLIT DATA
 x_train, X_test, y_train, y_test = train_test_split(X_scaled, y_onehot, test_size=0.2, random_state=42)
-print(f'X_train: {x_train.shape}')
-
 
 layer_1 = Layer(neurons=3, n_inputs=4,  activation='relu')
 layer_2 = Layer(neurons=4,  n_inputs=3, activation='relu')
 layer_3 = Layer(neurons=4,  n_inputs=4, activation='relu')
 layer_4 = Layer(neurons=3, n_inputs=4, activation='softmax')
 
-nn = NeuralNetwork(x_inputs=x_train, y_train=y_train, epochs=10000)
+nn = NeuralNetwork(x_inputs=x_train, y_train=y_train, epochs=100000)
 nn.add_layer(layer_1)
 nn.add_layer(layer_2)
 nn.add_layer(layer_3)
 nn.add_layer(layer_4)
 nn.train()
 
-print(y_test[0])
-
-
-print(nn.predict(X_test[0].reshape(1, -1)))
-
-#network = NeuralNetwork(x_inputs=x, y_train=[0, 1])
-#network.add_layer(layer_1)
-#network.add_layer(layer_2)
-#network.add_layer(layer_3)
-#network.propagate_network()
-
-
-
-#print(f'FINAL OUTPUT: {network.output()}')
-"""print(f'Loss: {network.loss_function()}')
-network.backpropgation()
-network.propagate_network()
-print(f'Loss: {network.loss_function()}')
-"""
-#network.train()
-#print(f'Tetha: {network.backpropgation()}')
-
-#print(f'Layer 1 OUTPUT {layer_1._values}') 
+for i in range(5):
+    index_real_value = encoder.inverse_transform(y_test[i].reshape(1, -1))
+    print(f'Expected value: {values[index_real_value[0][0]]}')
+    accuracy, index = nn.predict(X_test[i].reshape(1, -1))
+    print(f'{values[index]} with accuracy: {accuracy*100:.2f}%')
+    print('---'*30)
+    
--- neural_network.py
+++ neural_network.py
@@ -3,6 +3,8 @@ import numpy as np
 np.random.seed(42)
 
 
+#----- A class to represent the layer of a Neural Network, the atributes are the basic of a layer:
+# ---- number of neurons in the layer, activatio function, weights and biases of the layer
 class Layer():
     def __init__(self, neurons, n_inputs, activation):
         self.neurons = neurons
@@ -24,19 +26,20 @@ class Layer():
             elif self.activation == 'relu':
                 return np.maximum(0, weights_sum)
             elif self.activation == 'softmax':
-                exps = np.exp(weights_sum - np.max(weights_sum, axis=1, keepdims=True))  # estabilidad numérica
+                exps = np.exp(weights_sum - np.max(weights_sum, axis=1, keepdims=True))  # numerical stability
                 return exps / np.sum(exps, axis=1, keepdims=True)
         except Exception as e:
             print(f'Error: {e}')
-            print('No activation Function Found')
             print(f'Activation: {self.activation}')
     
     def forward_propagation(self, x_inputs):
+        #Propagation of the inputs through the layer
         self.x_inputs = x_inputs
         weights_sum = self.weighted_sum(self.x_inputs)
         self._values = self.activation_function(weights_sum)
 
     def values(self):
+        #Return the output values of the layer
         return self._values
     
     def softmax_derivate(self, output):
@@ -48,21 +51,24 @@ class Layer():
         
 
 
+#----- A class to represent the Neural Network, the arquitecture of how it works
 class NeuralNetwork():
     def __init__(self, x_inputs, y_train,  epochs=1000):
-        self._layers = []
+        self._layers = [] # list of all the layers
         self._output = []
         self.x_inputs = x_inputs 
         self.y_train = np.array(y_train)
         self.epochs = epochs
     
     def add_layer(self, layer):
+        # Add a layer to the neural network
         self._layers.append(layer)
     
     def layers(self):
         return self._layers    
     
     def propagate_network(self, inputs):
+        # Propagation of the inputs through all the newtork
         for i, layer in enumerate(self._layers):
             if i == 0:
                 layer.forward_propagation(x_inputs=inputs)
@@ -70,6 +76,7 @@ class NeuralNetwork():
                 layer.forward_propagation(x_inputs=self._layers[i-1].values())
             
     def output(self):
+        # Get the output of the Neural Network
         last_layer = len(self._layers) - 1
         self._output = self._layers[last_layer].values()
         return self._output
@@ -83,6 +90,7 @@ class NeuralNetwork():
         return - self.y_train / self._output
     
     def backpropgation(self):
+        # Backpropagation of the error through all the network
         last_layer = len(self._layers) - 1
         output = self.output()
         learning_rate = 0.001
@@ -91,6 +99,7 @@ class NeuralNetwork():
         for i in range(last_layer, -1, -1):
             #print(f'--------------LAYER {i}-----------------')
             
+            #Derivative of the activation function
             if self._layers[i].activation == 'relu':
                 values = self._layers[i].values()
                 de_activation = (values > 0).astype(float)
@@ -98,11 +107,13 @@ class NeuralNetwork():
             elif self._layers[i].activation == 'softmax':
                 de_activation = self._layers[i].softmax_derivate(output)
             
+            # Choose the inputs of the layer
             input_to_layer = self.x_inputs if i == 0 else self._layers[i-1].values()
             
+            # Calculate the delta
             #delta = delta * de_activation
-            delta = self._output - self.y_train
-            delta_weights = np.dot(delta.T, input_to_layer)
+            delta = self._output - self.y_train 
+            delta_weights = np.dot(delta.T, input_to_layer) 
             
             #print(f'Weights: {self._layers[i].weights}')
             
@@ -117,15 +128,21 @@ class NeuralNetwork():
             delta = np.dot(delta, self._layers[i].weights)
             
     def train(self):
+        # Loop to train the neural network,
+        # the goal is to minimize the loss function by updating the weights and biases of the network
         for i in range(self.epochs):
             self.propagate_network(self.x_inputs)
             self.backpropgation()
             
-            if i % 100 == 0:
-                print(f'Loss: {self.loss_function()}')
+            #if i % 1000 == 0:
+             #   print(f'Loss: {self.loss_function()}')
     
     def predict(self, x_test):
+        # if weights and biases are trained, we can predict the output of the neural network
         self.propagate_network(x_test)
-        return self.output()
+        values = list(self.output()[0])
+        value_predicted = max(values)
+        predicted_index = values.index(value_predicted)
+        return value_predicted, predicted_index
                             
             
